a8696979dbec6ba3f1629644ae4a0e5b53fbe472
iRedux <75001525+iRedux@users.noreply.github.com>
2022-05-30 06:44:32 +0200

    #001

--- app.js
+++ app.js
@@ -1,47 +1,39 @@
-const path = require('path');
 const express = require("express")
 const cors = require('cors');
 const axios = require("axios")
 const cheerio = require("cheerio")
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 
 app.use(cors({
     origin: '*'
 }));
 
 
+function stringToInt(num) {
+    return parseInt(num.replace(/,/g, ''))
+}
 
-app.get("/api", async (req, res) => {
+
+app.get("/", async (req, res) => {
     const response = await axios.get("https://coinmarketcap.com/currencies/vita-inu/")
     const $ = cheerio.load(response.data)
 
-    // $('.maxSupplyValue').each((index, element) => {
-    //     console.log(index)
-    // })
-    
     const maxSupply = $('.maxSupplyValue')[0].children[0].data
     const totalSupply = $('.maxSupplyValue')[1].children[0].data
-    const burntSupply = 999999999999999 - parseInt(totalSupply.replace(/,/g, ''))
-
-    // async function getPrice() {
-    // }
+    const burntSupply = 999999999999999 - stringToInt(totalSupply)
 
     res.json({
         data: {
-            maxSupply: parseInt(maxSupply.replace(/,/g, '')),
-            totalSupply: parseInt(totalSupply.replace(/,/g, '')),
+            maxSupply: stringToInt(maxSupply),
+            totalSupply: stringToInt(totalSupply),
             burntSupply: burntSupply
         }
     })
 })
 
-// All other GET requests not handled before will return our React app
-app.get("/", (req, res) => {
-    res.send("Hello!")
-})
 
   
 app.listen(port, () => {
-    console.log("port 3000")
+    console.log("Loaded...")
 })
