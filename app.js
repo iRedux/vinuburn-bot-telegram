c99a18ea1c1b93db74a0c06562e393890031275b
iRedux <75001525+iRedux@users.noreply.github.com>
2022-08-01 12:23:07 +0200

    first commit

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-const express = require("express")
-const axios = require("axios")
-const cheerio = require("cheerio")
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Telegram
-const TelegramBot = require('node-telegram-bot-api')
-// const token = 'XXX'
-const token = 'XXX' // test one
-const bot = new TelegramBot(token, {polling: true})
-const commands = ['total', 'max', 'burnt', 'info']
+const BOT_ID = "@ababs_coin_bot";
+const initialSupply = 1000000000000;
+const TelegramBot = require("node-telegram-bot-api");
+const token = "5422262184:AAHWugkFZL_XH33wiRA8HXgunSbSVfOtfMc";
+const bot = new TelegramBot(token, { polling: true });
+const commands = ["total", "max", "burnt", "info", "burned", "mcap", "circulating", "supply"];
+const supplyCommands = ["total", "max", "burnt", "burned", "circulating"]
 
-// 
-const stringToInt = (num) => parseInt(num.replace(/,/g, ''))
-const intToString = (num) => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-const capitalizeFirstLetter = (str) => str.replace(/^./, str[0].toUpperCase())
-
-const getSupply  = async() => {
-    const response = await axios.get("https://coinmarketcap.com/currencies/vita-inu/")
-    const $ = cheerio.load(response.data)
-
-    const maxSupply = stringToInt($('.maxSupplyValue')[0].children[0].data)
-    const totalSupply = stringToInt($('.maxSupplyValue')[1].children[0].data)
-    const burntSupply = 999999999999999 - totalSupply
-
-    return({
-        info: "Hello world",
-        total: intToString(totalSupply),
-        max: intToString(maxSupply),
-        burnt: intToString(burntSupply)
-    })
-}
+//
 
 bot.onText(/\/(.+)/, async (msg, match) => {
-    let req = match[1]
-    let res = null
+  let req = match[1];
+  let res = null;
 
+  req.includes(BOT_ID) && (req = req.replace(BOT_ID, ''))
     commands.includes(req)
-        ? res = (req === "info")
+    && (res =
+        req === "info"
             ? `This bot was made by @iredux as a part of vinuburn.com project.`
-            : `${capitalizeFirstLetter(req)} Supply: ${(await getSupply())[req]}`
-        : res = `Wrong command`
-    
-    bot.sendMessage(msg.chat.id, res);
+            : `qqqqq`)
+
+    res !== null && bot.sendMessage(msg.chat.id,res,{reply_to_message_id: msg.message_id})
+  
+});
+
+
+bot.onText(/iredux/, async (msg, match) => {
+  //https://vitescan.io/vs-api/address?address=vite_000000000000000000000000000000000000000595292d996d&tabFlag=txns&pageNo=1
+  // const response = await axios.get("https://vitescan.io/vs-api/address?address=vite_000000000000000000000000000000000000000595292d996d&tabFlag=txns&pageNo=1")
+  // const data = response.data.data
+  
+  // console.log(data)
+  // bot.sendMessage(msg.chat.id, data.accountInfo.address, {reply_to_message_id: msg.message_id})
+})
+
+bot.onText(/\/burnw/, (msg, match) => {
+  bot.sendMessage(msg.chat.id, "Burn wallet address: https://vitescan.io/address/vite_000000000000000000000000000000000000000595292d996d", {reply_to_message_id: msg.message_id})
 })
 
 
-app.listen(port, () => console.log("...."))
+app.listen(port, () => console.log("...."));
